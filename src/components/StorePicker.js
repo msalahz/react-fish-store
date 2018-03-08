7ba9cb177661c5e25cbeb4ccce8b4ea73e44405b
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-03-08 03:26:26 +0200

    Init layout

---
+++ src/components/StorePicker.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  // create an input property reference 
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // prevent for submit default
+    event.preventDefault();
+    // get input value
+    const storeName = this.myInput.value.value;
+    // redirect to store route
+    // history here is passed to the component from its Router parent component
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h1>Please Enter a Store</h1>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
